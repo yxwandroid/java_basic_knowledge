861b1835fd6a6d8d6ecf3b28092db61f893615db
wilson <yxw_android@163.com>
2020-07-17 17:19:04 +0800

    init

---
+++ src/com/wilson/java/thread/DeadLockDemo.java
@@ -0,0 +1,49 @@
+package com.wilson.java.thread;
+
+//死锁的场景
+// 当两个线程同时拥有一定的资源，但是都缺少对方手上的资源才能进行下一步动作，而去竞争对方的资源，从而都陷入等待的一种场景，这种场景被称为死锁。
+//例如：A、B线程在运行开始时分别持有a、b对象，A拥有a，对象a被A上锁了，B拥有b，对象b被B上锁了，此时，线程A在要往后运行需要对象b，而线程B要往后运行需要对象a，此时A、B线程都希望获得对方的资源，但是手上的资源都不愿拿出来，这个时候就形成了“僵局”，进入了死锁。
+// https://blog.csdn.net/u014039577/article/details/48808323
+// https://blog.csdn.net/xiao_ma_CSDN/article/details/78753242
+
+public class DeadLockDemo {
+    static Object o1 = new Object();
+    static Object o2 = new Object();
+
+    public static void main(String[] args) {
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (o1) {
+                    System.out.println("线程1锁o1");
+                    try {
+                        Thread.sleep(1000);
+                        synchronized (o2) {
+                            System.out.println("线程1锁o2");
+                        }
+                    } catch (Exception e) {
+
+                    }
+                }
+            }
+        }).start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (o2) {
+                    System.out.println("线程2锁o2");
+                    try {
+                        synchronized (o1) {
+                            System.out.println("线程2锁o1");
+                        }
+                    } catch (Exception e) {
+
+                    }
+                }
+            }
+        }).start();
+
+    }
+
+}
---
+++ src/com/wilson/java/thread/MultiThreadAddNum.java
@@ -0,0 +1,130 @@
+package com.wilson.java.thread;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+// 原文链接：https://blog.csdn.net/wzmde007/article/details/79641084
+public class MultiThreadAddNum {
+
+    int count = 0;
+
+    public static void main(String[] args) {
+//        method1();
+//        method2();
+        method3();
+
+    }
+
+
+    //创建五个个线程和五个任务进行操作
+    static public void method1() {
+        //new一个实现Runnable的类
+        MultiThreadAddNum test = new MultiThreadAddNum();
+        //创建5个任务
+        MyRunnable myRunnable1 = test.new MyRunnable();
+        MyRunnable myRunnable2 = test.new MyRunnable();
+        MyRunnable myRunnable3 = test.new MyRunnable();
+        MyRunnable myRunnable4 = test.new MyRunnable();
+        MyRunnable myRunnable5 = test.new MyRunnable();
+        //创建5个线程
+        new Thread(myRunnable1).start();
+        new Thread(myRunnable2).start();
+        new Thread(myRunnable3).start();
+        new Thread(myRunnable4).start();
+        new Thread(myRunnable5).start();
+    }
+
+    //使用类锁
+    class MyRunnable implements Runnable {
+
+        @Override
+        public void run() {
+            while (true) {
+
+                synchronized (MyRunnable.class) {
+                    if (count >= 1000) {
+                        break;
+                    }
+
+                    System.out.println(Thread.currentThread().getName() + ":count:" + (++count));
+                    //测试时，线程更容易切换
+                    Thread.yield();
+                    //使当前线程从运行状态转为就绪状态。CPU
+                }
+
+            }
+
+        }
+    }
+
+
+    //使用对象锁
+    static public void method2() {
+        MultiThreadAddNum test = new MultiThreadAddNum();
+        //创建5个任务
+        MyRunnable2 myRunnable2 = test.new MyRunnable2();
+        for (int i = 0; i < 5; i++) {
+            //创建5个线程
+            new Thread(myRunnable2).start();
+        }
+
+    }
+
+
+    class MyRunnable2 implements Runnable {
+
+        @Override
+        public void run() {
+            while (true) {
+
+                synchronized (this) {
+                    if (count >= 1000) {
+                        break;
+                    }
+
+                    System.out.println(Thread.currentThread().getName() + ":count:" + (++count));
+                    //测试时，线程更容易切换
+                    Thread.yield();
+                    //使当前线程从运行状态转为就绪状态。CPU
+                }
+
+            }
+
+        }
+    }
+
+
+    //使用对象锁
+    static public void method3() {
+        for (int i = 0; i < 20; i++) {
+            new Thread(new CountRunnable()).start();
+
+        }
+    }
+
+
+    //多线程安全的类
+    static AtomicInteger counter = new AtomicInteger(0);
+
+    public static int add() {
+        int andAdd = counter.getAndAdd(1);
+        return andAdd;
+    }
+
+
+    static class CountRunnable implements Runnable {
+
+        @Override
+        public void run() {
+            while (true) {
+                if (counter.get() > 1000) {
+                    break;
+                }
+                System.out.println(MultiThreadAddNum.add());
+                Thread.yield();
+            }
+
+        }
+    }
+
+
+}
